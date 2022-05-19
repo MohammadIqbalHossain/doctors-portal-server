1c5f4bef0184798e709c3bc6a8bebab011414af8
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-05-20 00:27:22 +0600

    displayed transactionId in the UI

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion, FindCursor } = require('mongodb');
+const { MongoClient, ServerApiVersion, FindCursor , ObjectId} = require('mongodb');
+const res = require('express/lib/response');
+const { send } = require('express/lib/response');
+const stripe = require('stripe')(process.env.STRIPR_SECRET_KEY);
 const port = process.env.PORT || 5000;
 const app = express()
 
@@ -29,9 +32,12 @@ const verifyToken = (req, res, next) => {
     next()
   });
 
-
-
 }
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  next();
+}) 
 
 
 async function run() {
@@ -41,13 +47,59 @@ async function run() {
     const servicesCollections = client.db("doctors_portal").collection("services");
     const bookingCollections = client.db("doctors_portal").collection("booking");
     const userCollections = client.db("doctors_portal").collection("users");
+    const doctorCollections = client.db("doctors_portal").collection("doctors");
+    const paymentsCollections = client.db("doctors_portal").collection("payments");
+    
+    const verifyAdmin = async (req, res, next) => {
+      const requester = req.decoded.email;
+      const initatorAccount = await userCollections.find({ email: requester });
+      if (initatorAccount.role === "admin") {
+        next();
+      }
+      else {
+        return res.status(403).send("Forbidden access");
+      }
+
+    }
+
+    app.post('/create-payment-intent',  async(req, res) => {
+      const service = req.body;
+      console.log(service);
+      const price = service.price;
+      const amount = price * 100;
+      console.log(amount);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "USD",
+        payment_method_types:["card"]
+      })
+      res.send({clientSecret: paymentIntent.client_secret})
+    })
+
+    app.patch("/booking/:id", async(req, res) => {
+      const id = req.params.id;
+      const payment = req.body;
+      console.log(payment);
+      const filter = {_id: ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          paid: true,
+          transactionId: payment.transactionId
+        }
+      }
+      const result = await paymentsCollections.insertOne(payment)
+      const updatedBooking = await bookingCollections.updateOne(filter, updateDoc)
+      res.send(updateDoc)
+    })
 
     app.get('/service', async (req, res) => {
       const query = {};
-      const cursor = servicesCollections.find(query);
+      const cursor = servicesCollections.find(query).project({ name: 1 });
       const result = await cursor.toArray();
       res.send(result);
     })
+
+
 
     app.post('/booking', async (req, res) => {
       const booking = req.body;
@@ -81,26 +133,20 @@ async function run() {
 
     app.get('/admin/:email', async (req, res) => {
       const email = req.params.email;
-      const user = await userCollections.findOne({email: email});
+      const user = await userCollections.findOne({ email: email });
       const isAdmin = user.role === 'admin';
-      res.send({admin: isAdmin})
+      res.send({ admin: isAdmin })
     })
 
-    app.put('/users/admin/:email', verifyToken, async (req, res) => {
+    app.put('/users/admin/:email', verifyToken, verifyAdmin, async(req, res) => {
       const email = req.params.email;
-      const requester = req.decoded.email;
-      const initatorAccount = await userCollections.find({ email: requester });
-      if (initatorAccount.role === "admin") {
+      console.log(email);
         const filter = { email: email };
         const updateDoc = {
           $set: { role: "admin" },
         };
         const result = await userCollections.updateOne(filter, updateDoc);
         return res.send({ result });
-      }
-      else {
-        return res.status(403).send("Forbidden access");
-      }
 
     })
 
@@ -119,6 +165,13 @@ async function run() {
       res.send({ result, token });
     })
 
+    app.get("/booking/:id",  async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await bookingCollections.findOne(query);
+      res.send(result);
+    })
+
     app.get('/available', async (req, res) => {
       const date = req.query.date;
 
@@ -134,6 +187,24 @@ async function run() {
         service.slots = available
       })
       res.send(services)
+    })
+
+    app.get("/doctor", async(req, res) => {
+      const result = await doctorCollections.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/doctor", verifyToken, verifyAdmin, async (req, res) => {
+      const data = req.body;
+      const result = await doctorCollections.insertOne(data);
+      res.send(result);
+    });
+
+    app.delete("/doctor/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = {email:email};
+      const result = await doctorCollections.deleteOne(filter);
+      res.send(result);
     })
 
 
